d149fa777dcc3189e2aa98d2e51a946fd528e510
Matt Tarasoff <tarasoff.matt@gmail.com>
2024-12-13 16:31:05 -0500

    added a profile pic for testing and refined the Card style.

--- src/app/App.js
+++ src/app/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import Card from '../features/cards/Card';
 
-import cardSource from '../data/assets/card-image.jpg';
+import cardMedia from '../data/assets/card-image.jpg';
+import profilePic from '../data/assets/morty.jpg';
 
 // const cardSource = '../data/assets/card-image.jpg';
 
 function App() {
   return (
     <div className="App">
-      <Card image={cardSource}/>
+      <Card cardMedia={cardMedia} profilePic={profilePic} />
     </div>
   );
 }
--- src/features/cards/Card.js
+++ src/features/cards/Card.js
@@ -1,24 +1,27 @@
 import { React } from 'react';
 
-export default function Card({image}) {
+export default function Card({cardMedia, profilePic}) {
     return (
         <div className='card'>
             <div className='card-header'>
                 <div className='avatar'>
-                    <img src='https://via.placeholder.com/50' alt='placeholder' />
+                    <img src={profilePic} alt='placeholder' />
                 </div>
                 <h2>Card Header</h2>
             </div>
             <div className='card-container'>
                 <div className='card-meadia'>
-                    <img src={image} alt='placeholder' />
+                    <img src={cardMedia} alt='placeholder' />
                 </div>
                 <div className='content-buttons'>
                     <button className='like-buttons'>left button</button>
                     <button className='like-buttons'>right button</button>
                 </div>
                 <div className='card-text'>
-                    <p>Some information about the picture in the content div.</p>
+                    <p>Some information about the picture in the content div.  This is just a bunch of words to 
+                        take up space and make the card look like it has some content.  This is just a bunch of words to
+                        take up space and make the card look like it has some content.  This is just a bunch of words to
+                    </p>
                 </div>
             </div>
           
